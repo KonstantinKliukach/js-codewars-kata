8b3c6a69a2169dde0ec7a4839f19ff032586ef0a
JFrost <k.kliukach@gmail.com>
2019-10-17 10:13:17 +0300

    findOutliner

--- highestScoringWord.js
+++ highestScoringWord.js
@@ -1,3 +1,16 @@
+/*
+  Description:
+  Given a string of words, you need to find the highest scoring word.
+
+  Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+  You need to return the highest scoring word as a string.
+
+  If two words score the same, return the word that appears earliest in the original string.
+
+  All letters will be lowercase and all inputs will be valid.
+*/
+
 function high(string){
   const alphabetArr = [...Array(26)].map((el, i)=>String.fromCharCode(i + 97));
   const words = string.split(' ');
